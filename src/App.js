9896bf904b49946e4a1f731a3b0cb6c6a958fcad
crossbowburrito <crossbowburrito@gmail.com>
2023-12-28 15:09:21 -0600

    WIP

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Footer from './Footer';
 import ProjectTile from './ProjectTile';
 
+
 function App() {
   const [projects, setProjects] = useState([]);
 
--- src/ProjectTile.js
+++ src/ProjectTile.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './ProjectTile.css'; // Create and import corresponding CSS file
 
+function formatRepoName(name) {
+    return name
+      // Split on hyphens
+      .split('-')
+      // Capitalize the first letter of each word
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      // Join the words back into a string
+      .join(' ');
+  }
+
+
 function ProjectTile({ project }) {
     return (
       <div className="project-tile">
-        <h3>{project.name}</h3>
+        <h3>{formatRepoName(project.name)}</h3>
         <p>{project.description}</p>
         <div className="project-tile-buttons">
           <a href={`/projects/${project.name}`} className="btn project-page-btn">Project Page</a>
